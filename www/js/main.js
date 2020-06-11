b689ce5b06151eb7848f04a5eda23442b0ad73c2
Abe Hinako <h-abe@griffin-net.co.jp>
2020-06-11 14:31:05 +0900

    2020/06/11 14:30

--- www/js/main.js
+++ www/js/main.js
@@ -2,9 +2,10 @@
 
 {
   let currentIndex = 0;
-    let tapCount = 0;
+  let tapCount = 0;
+  let t; //画像の切り替え時間
 
-    const colors = [
+    let colors = [
         {img: 'image/pic00.png', name: "画像0"},
         {img: "image/pic01.png", name: "画像1"},
         {img: "image/pic02.png", name: "画像2"},
@@ -14,11 +15,11 @@
         {img: "image/pic06.png", name: "画像6"},
         {img: "image/pic07.png", name: "画像7"},
       ];
-      
-  document.addEventListener('init', (event) => {
+  
+  document.addEventListener('show', (event) => {
     let page = event.target;
-    const images = document.querySelector('.images');    
-        
+    const images = document.getElementById('images');
+
     if(page.matches('#top-page')){
       
       if(images.hasChildNodes()){
@@ -32,36 +33,110 @@
         const p = document.createElement('p');
         img.src = color.img;
         p.textContent = color.name;
-        li.appendChild(img);
-        console.log(img);        
+        li.appendChild(img);       
         li.appendChild(p);
-        console.log(p);
+        const btn = document.createElement('ons-button');
+        btn.textContent = 'remove';
+        li.classList.add('all');
+        li.appendChild(btn);
         images.appendChild(li);
-        console.log(li);
+        
+        btn.onclick = function() {
+          for(let i = 0; i < colors.length; i++){
+            if(p.textContent === colors[i].name){
+              colors.splice(i, 1);
+            }
+          };
+          li.parentNode.removeChild(li);
+          console.log(colors);
+        }
       });
-      // console.log(images);
-      console.log(document.querySelector('.images-container'));
       
 
-      page.querySelector('#btn').onclick = function() {
-        document.querySelector('#navigator').pushPage('slide.html');
-        currentIndex = 0;
-        tapCount = 0;
-        lists.splice(0);
-        selectNames.splice(0);
+      page.querySelector('#start').onclick = function() {
+        document.querySelector('#navigator').pushPage('levels.html');
+        console.log(colors);
       };
 
       page.querySelector('#add').onclick = function() {
         document.querySelector('#navigator').pushPage('add.html');
       };
+    }
+  });
+      
+  document.addEventListener('init', (event) => {
+    let page = event.target;
+    
+    if(page.matches('#levels-page')){
+      
+      page.querySelector('#level1').onclick = function() {
+        t = 1000;
+        Initialization();
+        document.querySelector('#navigator').pushPage('slide.html');
+        console.log('level1');
+      };
 
-    }else if(page.matches('#add-page')){
-      page.querySelector('#plus').onclick = function() {
-        document.querySelector('#navigator').pushPage('top.html');
+      page.querySelector('#level2').onclick = function() {
+        t = 500;
+        Initialization();
+        document.querySelector('#navigator').pushPage('slide.html');
+        console.log('level2');
+      };
+
+      page.querySelector('#level3').onclick = function() {
+        t = 100;
+        Initialization();
+        document.querySelector('#navigator').pushPage('slide.html');
+        console.log('level3');
+
+      };
+
+      page.querySelector('#level4').onclick = function() {
+        t = 10;
+        Initialization();
+        document.querySelector('#navigator').pushPage('slide.html');
+        console.log('level4');
+      };
+      page.querySelector('#random').onclick = function() {
+        const levelNumber = Math.floor(Math.random() * 4 + 1);
+        page.querySelector('#level' + String(levelNumber)).click();
       };
 
 
+
+    }else if(page.matches('#add-page')){
+      const file = document.getElementById('file');
+      const imageName = document.getElementById('image-name');
+      const addImage = document.getElementById('add-image');
+      let imageFileName;
+
+      file.addEventListener('change', (e) => {
+        console.log(e.target.files);
+        let imageFile = e.target.files;
+        let reader = new FileReader();
+        reader.readAsDataURL(imageFile[0]);
+        reader.onload = function(){
+          imageFileName = reader.result;
+          addImage.src = reader.result;
+        }
+      }, false);
+
+      page.querySelector('#plus').onclick = function() {
+        if(file.value === '' || imageName.value === '' ){
+          return;
+        }
+        colors.push({img: imageFileName, name: imageName.value});
+        console.log(colors);
+        document.querySelector('#navigator').resetToPage('top.html', {animation: "default"});
+      }
+
+
+
+
     }else if(page.matches('#slide-page')){
+      shuffles = shuffle([...colors]);
+      shuffleimages = [...shuffles];
+      console.log(shuffleimages);
       const main = document.getElementById('main');
       main.src = shuffleimages[currentIndex].img;
       play();
@@ -69,8 +144,14 @@
         clearTimeout(timeoutId);
       }
 
+
+
     }else if(page.matches('#select-page')){
       setName();
+
+      page.querySelector('#stop').onclick = function() {
+        document.querySelector('#navigator').resetToPage('top.html', {animation: "default"});
+      };
 
       page.querySelector('#reset').onclick = function() {
         tapCount = 0;
@@ -85,23 +166,26 @@
         selectNames.splice(0);
       }
 
+      
+      
       page.querySelector('#check').onclick = function() {
         if(tapCount !== colors.length){
           return;
         }
-        document.querySelector('#navigator').pushPage('last.html');
+        document.querySelector('#navigator').pushPage('last' + '.html');
       };
 
     }else if(page.matches('#last-page')){
 
       const p = document.querySelector('#judge');
-      shuffleimages.forEach((shuffleimage, index) => {
-        if(shuffleimage.name === selectNames[index].name){
-          p.textContent = "完璧";
-        }else{
+      for(let i = 0; i < shuffleimages.length; i++){
+        if(shuffleimages[i].name !== selectNames[i].name){
           p.textContent = "残念";
+          break;
+        }else{
+          p.textContent = "完璧";
         }
-      });
+      }
 
       selectNames.forEach((selectName) => {
         for(let i = 0; i < colors.length; i++){
@@ -121,7 +205,7 @@
         answer.appendChild(li);
       });
 
-      const myAnswer = document.getElementById('my-answer');
+      const myAnswer = document.getElementById('your-answer');
       selectNames.forEach((selectName) => {
         const li = document.createElement('li');
         li.classList.add('row');
@@ -139,20 +223,25 @@
     }  
   });
 
+  function Initialization(){
+    currentIndex = 0;
+    tapCount = 0;
+    lists.splice(0);
+    selectNames.splice(0);
+  }
 
-
-  let timeoutId
+  let timeoutId;
   function play(){
     if(currentIndex == shuffleimages.length - 1){
       setTimeout(() => {
         document.querySelector('#navigator').pushPage('select.html');
-      }, 100);
+      }, t);
       return;
     }
     timeoutId = setTimeout(() => {
       main.src = shuffleimages[currentIndex].img;
       play();
-    }, 100);
+    }, t);
     currentIndex++;
   }
 
@@ -167,8 +256,9 @@
     return arr;
   }
 
-  const shuffles = shuffle([...colors]);
-  const shuffleimages = [...shuffles];
+  
+  let shuffles = [];
+  let shuffleimages = [];
 
 
   let selectNames = [];
